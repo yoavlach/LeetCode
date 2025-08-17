b0bd6a24ef1b5bf6a7f3470967475edf876e45b8
Yoav Lach <158746547+yoavlach@users.noreply.github.com>
2025-08-18 01:34:04 +0300

    Time: 139 ms (6.34%) | Memory: 27.5 MB (10.40%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,21 +1,18 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         i, j = 0, k - 1
-        curr_avg, max_avg = None, -2 ** 31
+        curr_sum, max_sum = None, -2 ** 31
         while j in range(len(nums)):
-            if curr_avg is None:
-                curr_avg = 0
+            if curr_sum is None:
+                curr_sum = 0
                 for l in range(i, j + 1):
-                    curr_avg += nums[l]
-                curr_avg /= k
+                    curr_sum += nums[l]
             else:
-                curr_avg *= k
-                curr_avg -= nums[i - 1]
-                curr_avg += nums[j]
-                curr_avg /= k
-            if curr_avg > max_avg:
-                max_avg = curr_avg
+                curr_sum -= nums[i - 1]
+                curr_sum += nums[j]
+            if curr_sum > max_sum:
+                max_sum = curr_sum
             i += 1
             j += 1
-        return max_avg
+        return max_sum / k
             

5803e520a150da7d809e072955269a2aed79994b
Yoav Lach <158746547+yoavlach@users.noreply.github.com>
2025-09-06 01:25:02 +0300

    Time: 35 ms (45.06%) | Memory: 20.4 MB (46.57%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        nums_len = len(nums)
         product = 1
-        output = [0] * len(nums)
+        output = [0] * nums_len
         output[0] = 1
-        for i in range(1, len(nums) + 1):
+        for i in range(1, nums_len + 1):
             output[i - 1] = product
             product *= nums[i - 1]
         product = 1
-        for i in range(len(nums), 0, -1):
+        for i in range(nums_len, 0, -1):
             output[i - 1] *= product
             product *= nums[i - 1]
         return output

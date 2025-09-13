17b1e4915bb9072d52e463cb8ae0d06a0caf0439
Yoav Lach <158746547+yoavlach@users.noreply.github.com>
2025-09-13 13:57:04 +0300

    Time: 6 ms (46.04%) | Memory: 18.9 MB (49.56%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,15 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zeros_counter = 0
-        num_len = len(nums)
         i = 0
-        while i in range(num_len):
-            if i >= num_len - zeros_counter:
-                break
-            if nums[i] == 0:
-                nums.pop(i)
-                zeros_counter += 1
+        nums_len = len(nums)
+        while i < nums_len:
+            curr_num = nums[i]
+            if curr_num == 0:
+                del nums[i]
+                nums.append(0)
+                nums_len -= 1
             else:
                 i += 1
-        nums.extend([0] * zeros_counter)

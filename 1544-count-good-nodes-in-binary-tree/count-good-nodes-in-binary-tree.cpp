c02bca4cfe8b1cbf86934c9d79ba34d9ebb6b431
Yoav Lach <158746547+yoavlach@users.noreply.github.com>
2026-03-10 15:39:58 +0200

    Time: 172 ms (5.30%) | Memory: 134.4 MB (5.10%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int goodNodes(TreeNode* root)
+    {
+        bool inTreeRoot = seen.size() == 0;
+        int goodNodesCounter = 0, prevPathMax = 0;
+        if (inTreeRoot)
+            currPathMax = root->val;
+        prevPathMax = currPathMax;
+        seen.insert(root);
+        for (auto i : { root->left, root->right })
+        {
+            currPathMax = prevPathMax;
+            if (inTreeRoot)
+                currPathMax = root->val;
+            if (i && seen.find(i) == seen.end())
+            {
+                if (i->val >= currPathMax)
+                {
+                    currPathMax = i->val;
+                }
+                goodNodesCounter += goodNodes(i);
+            }
+        }
+        if (root->val >= prevPathMax || inTreeRoot)
+            goodNodesCounter++;
+        return goodNodesCounter;
+    }
+private:
+    set<TreeNode*> seen;
+    int currPathMax = 0;
+};

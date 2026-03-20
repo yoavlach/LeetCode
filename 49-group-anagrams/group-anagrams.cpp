98acd3ed071d4d280d51ad353f7d6282f09baf5d
Yoav Lach <158746547+yoavlach@users.noreply.github.com>
2026-03-20 02:14:52 +0200

    Time: 39 ms (18.08%) | Memory: 32.6 MB (13.77%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,26 +3,20 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) 
     {
         vector<vector<string>> anagrams;
-        map<char, int> curr;
-        map<map<char, int>, vector<string>> letterCounterAndAnagrams;
+        vector<int> letterCounter(26);
+        map<vector<int>, vector<string>> letterCounterAndAnagrams;
         for (string i : strs)
         {
-            curr.clear();
+            fill(letterCounter.begin(), letterCounter.end(), 0);
             for (char j : i)
-            {
-                if (curr.find(j) != curr.end())
-                    curr.find(j)->second++;
-                else
-                    curr[j] = 1;
-            }
-            if (letterCounterAndAnagrams.find(curr) != letterCounterAndAnagrams.end())
-                letterCounterAndAnagrams.find(curr)->second.push_back(i);
+                letterCounter[j - 97]++;
+            if (letterCounterAndAnagrams.find(letterCounter) != letterCounterAndAnagrams.end())
+                letterCounterAndAnagrams.find(letterCounter)->second.push_back(i);
             else
-                letterCounterAndAnagrams[curr] = { i };
+                letterCounterAndAnagrams[letterCounter] = { i };
         }
         for (auto i : letterCounterAndAnagrams)
             anagrams.push_back(i.second);
         return anagrams;
     }
-
 };

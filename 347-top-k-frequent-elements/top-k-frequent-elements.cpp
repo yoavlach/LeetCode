6636f625563563d147326412451cb0799f426000
Yoav Lach <158746547+yoavlach@users.noreply.github.com>
2026-03-21 02:54:56 +0200

    Time: 33 ms (9.10%) | Memory: 26.4 MB (10.25%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> appearanceCounter;
+        vector<int> res(k);
+        vector<set<int>> appearAsIndex(nums.size() + 1);
+        int insertCounter = 0;
+        for(int i : nums)
+        {
+            auto curr = appearanceCounter.find(i);
+            if(curr == appearanceCounter.end())
+            {
+                appearanceCounter[i] = 1;
+                appearAsIndex[1].insert(i);
+            }
+            else
+            {
+                appearAsIndex[curr->second].erase(i);
+                curr->second++;
+                appearAsIndex[curr->second].insert(i);
+            }
+        }
+        for(int i = appearAsIndex.size() - 1; i >= 0 && insertCounter < k; i--)
+        {
+            for(int j : appearAsIndex[i])
+            {
+                res[insertCounter] = j;
+                if(++insertCounter == k)
+                    break;
+            }
+        }
+        return res;
+    }
+};

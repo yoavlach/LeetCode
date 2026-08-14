7b023d7b84f81a8db4567304924aebd0bec0eba0
Yoav Lach <yoavlach@gmail.com>
2026-08-14 18:57:23 +0300

    Time: 5 ms (88.50%) | Memory: 14.2 MB (73.88%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,25 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        nums_and_appearances = {}
+
+        el_and_appearance = {}
+
         for i in nums:
-            if i in nums_and_appearances:
-                nums_and_appearances[i] += 1
+            if i in el_and_appearance:
+                el_and_appearance[i] += 1
             else:
-                nums_and_appearances[i] = 1
-        largest_k = []
-        for i in nums_and_appearances:
-            curr_appearance = nums_and_appearances[i]
-            to_insert_from = -1
-            for j in range(len(largest_k)):
-                if to_insert_from == -1 and curr_appearance > nums_and_appearances[largest_k[j]]:
-                    to_insert_from = j
-            if to_insert_from != -1 or len(largest_k) < k:
-                if to_insert_from == -1:
-                    largest_k.append(i)
-                else:
-                    largest_k.insert(to_insert_from, i)
-                if len(largest_k) > k:
-                    largest_k.pop()
-        return largest_k
-        
+                el_and_appearance[i] = 1
+
+        return sorted(el_and_appearance, key=el_and_appearance.get, reverse=True)[:k]

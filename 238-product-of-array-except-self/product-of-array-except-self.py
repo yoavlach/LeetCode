ddfb08ef9f2495e2fe4e54689d65f36ba571cb0c
Yoav Lach <yoavlach@gmail.com>
2026-08-16 23:05:39 +0300

    Time: 34 ms (41.39%) | Memory: 21.1 MB (33.21%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,16 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums_len = len(nums)
-        product = 1
-        output = [0] * nums_len
-        output[0] = 1
-        for i in range(1, nums_len + 1):
-            output[i - 1] = product
-            product *= nums[i - 1]
-        product = 1
-        for i in range(nums_len, 0, -1):
-            output[i - 1] *= product
-            product *= nums[i - 1]
-        return output
-        
+        # [-1,1,0,-3,3] -> [0,0,9,0,0]
+
+        overall_mul = 1
+        zero_counter = 0
+        for i in nums:
+            if i == 0:
+                zero_counter += 1
+            else:
+                overall_mul *= i
+
+        if zero_counter == len(nums):
+            overall_mul = 0
+
+        res = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                if zero_counter > 1:
+                    res.append(0)
+                else:
+                    res.append(overall_mul)
+            else:
+                if zero_counter > 0:
+                    res.append(0)
+                else:
+                    res.append(overall_mul / nums[i])
+        return res
